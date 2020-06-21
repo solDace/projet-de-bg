3c125eee4f7f0739851173d3d4d7a9fe3695b923
Tomitom13 <60874830+Tomitom13@users.noreply.github.com>
2020-06-21 16:47:14 +0200

    Ajout des check et des menus débuts/fin

--- Projet N1/Code/template.c
+++ Projet N1/Code/template.c
@@ -82,3 +82,120 @@ strcat(text, ScoreText);
 	afficheChaine(text,16, 8*LargeurFenetre/9 - taille/2, HauteurFenetre/20 - 5);
 
 }
+
+/**
+ * \fn void Display_TestScore(int score)
+ * \brief Affichage du Score actuel
+ *
+ * \param score score ectuel
+ * \return none
+ */
+
+void Display_TestTime(time_t old, time_t new){
+
+int taille;
+char ScoreText[10], text[20];
+sprintf(text, "Time : ");
+unsigned long secondes = (unsigned long) difftime( new, old );
+sprintf(ScoreText, "%ld s", secondes);
+
+
+strcat(text, ScoreText);
+
+	couleurCourante(120,138,89);
+	taille = tailleChaine(text, 16);
+	afficheChaine(text,16, 8*LargeurFenetre/9 - taille/2, 2 * HauteurFenetre/20 - 5);
+
+}
+
+void Display_TestBegin(){
+
+	static float xCoin1, yCoin1, xCoin2, yCoin2;
+
+	xCoin1 = LargeurFenetre/8;
+	yCoin1 = (HauteurFenetre - HauteurFenetre/10)/6;
+	xCoin2 = 7*LargeurFenetre/8;
+	yCoin2 = 5*(HauteurFenetre - HauteurFenetre/10)/6;
+
+	couleurCourante(220,220,220);
+
+	rectangle( xCoin1,  yCoin1,  xCoin2,  yCoin2);
+
+	int taille;
+
+		couleurCourante(120,138,89);
+		taille = tailleChaine("Cliquez sur l'ecran pour commencer", 24);
+		afficheChaine("Cliquez sur l'ecran pour commencer",24, (LargeurFenetre - taille)/2, (HauteurFenetre - HauteurFenetre/10)/2-5);
+}
+
+void Display_TestEnd(int score){
+
+	static float xCoin1, yCoin1, xCoin2, yCoin2;
+	char ScoreText[10], text[30];
+
+	sprintf(text, "Votre score est de : ");
+	sprintf(ScoreText, "%d", score);
+
+
+	strcat(text, ScoreText);
+
+
+	xCoin1 = LargeurFenetre/8;
+	yCoin1 = (HauteurFenetre - HauteurFenetre/10)/6;
+	xCoin2 = 7*LargeurFenetre/8;
+	yCoin2 = 5*(HauteurFenetre - HauteurFenetre/10)/6;
+
+	couleurCourante(220,220,220);
+
+	rectangle( xCoin1,  yCoin1,  xCoin2,  yCoin2);
+
+	int taille;
+
+	couleurCourante(120,138,89);
+	taille = tailleChaine(text, 24);
+	afficheChaine(text,24, (LargeurFenetre - taille)/2, (HauteurFenetre - HauteurFenetre/10)/2-5);
+
+	couleurCourante(240,240,240);
+	rectangle( 2*LargeurFenetre/8,  3*(HauteurFenetre - HauteurFenetre/10)/12,   3*LargeurFenetre/8,   5*(HauteurFenetre - HauteurFenetre/10)/12);
+	couleurCourante(140,140,140);
+	taille = tailleChaine("Quitter", 15);
+	afficheChaine("Quitter", 15, 5*LargeurFenetre/16- taille/2,  4*(HauteurFenetre - HauteurFenetre/10)/12);
+
+	couleurCourante(240,240,240);
+	rectangle( 5*LargeurFenetre/8,  3*(HauteurFenetre - HauteurFenetre/10)/12,   6*LargeurFenetre/8,   5*(HauteurFenetre - HauteurFenetre/10)/12);
+	couleurCourante(140,140,140);
+	taille = tailleChaine("Rejouer", 15);
+	afficheChaine("Rejouer", 15, 11*LargeurFenetre/16 - taille/2,   4*(HauteurFenetre - HauteurFenetre/10)/12);
+
+}
+
+int Check_TestBegin(){
+
+  int check=0;
+
+  if(abscisseSouris() <= LargeurFenetre && abscisseSouris() >= 0 && ordonneeSouris() <= HauteurFenetre && ordonneeSouris() >= 0){
+    check=1;
+  }
+  return check;
+}
+
+
+int Check_TestQuitter(){
+
+  int check=0;
+
+  if(abscisseSouris() <= 3*LargeurFenetre/8 && abscisseSouris() >= 2*LargeurFenetre/8 && ordonneeSouris() <= 5*((HauteurFenetre - HauteurFenetre/10)/12) && ordonneeSouris() >=  3*(HauteurFenetre - HauteurFenetre/10)/12){
+    check=1;
+  }
+  return check;
+}
+
+int Check_TestRejouer(){
+
+  int check=0;
+
+  if(abscisseSouris() <= 6*LargeurFenetre/8 && abscisseSouris() >=  5*LargeurFenetre/8 && ordonneeSouris() <= 5*((HauteurFenetre - HauteurFenetre/10)/12) && ordonneeSouris() >=  3*(HauteurFenetre - HauteurFenetre/10)/12){
+    check=1;
+  }
+  return check;
+}
