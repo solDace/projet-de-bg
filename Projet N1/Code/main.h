2de87ae1a20fa16e06c4b36aeeae1d5ac61ce98f
Bajiio <60874928+Bajiio@users.noreply.github.com>
2020-06-21 18:23:07 +0200

    Update main.h

--- Projet N1/Code/main.h
+++ Projet N1/Code/main.h
@@ -15,19 +15,21 @@
 #include <time.h> // Pour pouvoir utiliser srand() et time()
 
 
-
 #include "../lib/BmpLib.h" // Cet include permet de manipuler des fichiers BMP
 #include "../lib/GfxLib.h" // Seul cet include est necessaire pour faire du graphique
 #include "../lib/ESLib.h" // Pour utiliser valeurAleatoire()
 
+
 // Largeur et hauteur par defaut d'une image correspondant a nos criteres
 #define LargeurFenetre 1000
 #define HauteurFenetre 800
-
+#define MAX_TRY 200
+#define MAX_NAME 30
+#define MAX_PLAYERS 50
 
 typedef struct  coord{
-int x;
-int y;
+	int x;
+	int y;
 }coord;
 
 void traceMenu();
@@ -48,3 +50,16 @@ int Check_Circle(coord circle);
 int Check_TestBegin();
 int Check_TestQuitter();
 int Check_TestRejouer();
+
+
+void resTest1(char pName[MAX_NAME]);
+void resTest2(char pName[MAX_NAME]);
+void resTest3(char pName[MAX_NAME]);
+void resTest4(char pName[MAX_NAME]);
+void resTest5(char pName[MAX_NAME]);
+void resTest6(char pName[MAX_NAME]);
+void PageResultat();
+
+
+int ReadScore(int score[MAX_PLAYERS], char pName[MAX_NAME],char files[MAX_NAME]);
+int SaveScore(int score, char pName[MAX_NAME],char files[MAX_NAME]);
