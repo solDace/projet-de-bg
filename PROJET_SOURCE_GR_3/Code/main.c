e94df3080935373daf0a07e90ba40244b84b0e9e
Matthis BERNET <60874771+MattiZZZ241@users.noreply.github.com>
2020-06-25 23:27:59 +0200

    COMMIT VERSION FINALE
    PAS DE .O SAVE REMPLI

---
+++ PROJET_SOURCE_GR_3/Code/main.c
@@ -0,0 +1,819 @@
+#include "main.h"
+
+/**
+* \file main.c
+* \brief fonction main et gestionEvenement
+* \author Thomas.B
+* \version 1.0
+* \date 20 Juin 2020
+*
+* Fonctions d'affichage servant de template pour les différentes pages de tests
+*
+*/
+
+
+
+
+/* La fonction de gestion des evenements, appelee automatiquement par le systeme
+des qu'une evenement survient */
+void gestionEvenement(EvenementGfx evenement);
+
+
+
+int main(int argc, char **argv)
+{
+	initialiseGfx(argc, argv);
+
+	prepareFenetreGraphique("GfxLib", LargeurFenetre, HauteurFenetre);
+
+	/* Lance la boucle qui aiguille les evenements sur la fonction gestionEvenement ci-apres,
+	qui elle-meme utilise fonctionAffichage ci-dessous */
+	lanceBoucleEvenements();
+
+	return 0;
+}
+
+/* variable désignant la page active
+-2 = Connexion
+0 = Menu principal
+1 = Menu Mémoire
+2 = Menu Réflex
+21 = Test Cercle
+3 = Menu Syncrhonisation
+4 = Menu Résultats
+*/
+int page = -2;
+
+struct timeval time_debut;
+struct timeval time_actuel;
+struct timeval time_niveau;
+
+
+coord circle;
+int check = 0; // Check:  0=pas cliqué
+int score=-1;
+int partie=0;
+int saving=0;
+
+int listen=1;
+
+char name[MAX_NAME]="";
+char text;
+
+int pattern[MAX_PATTERN];
+int niveau = 1;
+int avancee;
+
+coul couleurecrit[4];
+char tabbleu[4];
+char tabrouge[5];
+char tabvert[4];
+char tabnoir[4];
+char ecritcouleur[4][6]={"bleu","vert","rouge","noir"};
+int c , ce;
+
+int active;
+int rdtime;
+
+int **matrice = NULL;
+int tab[3][4],NbrOneMatrice = 0,win_game = 0;
+
+/* La fonction de gestion des evenements, appelee automatiquement par le systeme
+des qu'une evenement survient */
+void gestionEvenement(EvenementGfx evenement)
+{
+	static bool pleinEcran = false; // Pour savoir si on est en mode plein ecran ou pas
+	static DonneesImageRGB *image = NULL; // L'image a afficher au centre de l'ecran
+
+
+
+	switch (evenement)
+	{
+		case Initialisation:
+		active =rand()%2 +4;
+		rdtime =rand()%3;
+		/* Le message "Initialisation" est envoye une seule fois, au debut du
+		programme : il permet de fixer "image" a la valeur qu'il devra conserver
+		jusqu'a la fin du programme : soit "image" reste a NULL si l'image n'a
+		pas pu etre lue, soit "image" pointera sur une structure contenant
+		les caracteristiques de l'image "imageNB.bmp" */
+
+		// Configure le systeme pour generer un message Temporisation
+		// toutes les 20 millisecondes
+		demandeTemporisation(20);
+		//initecritcouleur(ecritcouleur, tabbleu, tabrouge, tabvert, tabnoir);
+		initcouleurecrit(couleurecrit);
+		break;
+
+		case Temporisation:
+
+		// il faut redessiner la fenetre :
+		rafraichisFenetre();
+		break;
+
+		case Affichage:
+
+		// On part d'un fond d'ecran blanc
+		effaceFenetre (255, 255, 255);
+
+		switch (page) {
+
+			case -2:
+			if(partie==0){
+				Display_MsgAccueil();
+			}
+			if(partie==1){
+
+				Display_MsgBienvenue(name);
+			}
+			break;
+			case 0:
+			traceMenu(name);
+			break;
+			case 1:
+			menuMem();
+			break;
+			case 11:
+			gettimeofday(&time_actuel, NULL);
+
+			Display_TestTop();
+			Display_TestName("Test du Simon Says");
+			if(partie==0){
+				Display_TestBegin(2);
+			}
+			else if(partie==1){
+
+				Display_TestMemoire(partie);
+
+
+				if (pattern[avancee]!=0) {
+					Display_ColorButtons(pattern[avancee]);
+				}
+				else{
+					partie++;
+					avancee=0;
+				}
+				if(/* (unsigned long) difftime( time_actuel, time_niveau )*/ (long)(  ((( time_actuel.tv_sec - time_niveau.tv_sec) * 1000000) + time_actuel.tv_usec) - (time_niveau.tv_usec))/1000000 >= 1){
+					Display_ColorButtons(0);
+
+
+
+
+					if((long)(  ((( time_actuel.tv_sec - time_niveau.tv_sec) * 1000000) + time_actuel.tv_usec) - (time_niveau.tv_usec)) >= 1200000 ){
+						if(avancee!=MAX_PATTERN){
+							avancee++;
+						}
+						else{
+							partie++;
+							avancee = 0;
+						}
+						gettimeofday(&time_niveau, NULL);
+					}
+				}
+
+
+				Display_TestScore(score);
+
+			}
+			else if(partie==2){
+
+
+
+				Display_ColorButtons(0);
+
+				Display_TestScore(score);
+
+			}
+			else{
+				Display_TestEnd(score);
+				if(saving==0){
+					SaveScore(score,name,"../Save/TestSimon.txt");
+					saving=1;
+				}
+
+			}
+			Display_TestMemoire(partie);
+			break;
+			case 12:
+			gettimeofday(&time_actuel, NULL);
+			Display_TestTop();
+			Display_TestName("Test du Pattern");
+			if(partie==0){
+				Display_TestBegin(1);
+			}
+			else if(partie==1){
+
+
+
+				init_carre(matrice);
+
+
+
+				Display_TestScore(score);
+				Display_TestTime(time_debut, time_actuel);
+
+
+
+				if(  (long)(  ((( time_actuel.tv_sec - time_niveau.tv_sec) * 1000000) + time_actuel.tv_usec) - (time_niveau.tv_usec))/1000000 >= 1){
+					partie++;
+				}
+			}
+			else if(partie==2){
+
+				carre_gris(matrice,tab);
+
+
+
+
+
+				Display_TestScore(score);
+				Display_TestTime(time_debut, time_actuel);
+				if(  (long)(  ((( time_actuel.tv_sec - time_debut.tv_sec) * 1000000) + time_actuel.tv_usec) - (time_debut.tv_usec))/1000000 >= 30){
+					partie++;
+				}
+			}
+
+			else{
+				Display_TestEnd(score);
+				if(saving==0){
+					SaveScore(score,name,"../Save/TestPattern.txt");
+					saving=1;
+				}
+			}
+			break;
+
+			case 2:
+			menuFlex();
+			break;
+			case 21:
+			Display_TestTop();
+			Display_TestName("Test du cercle");
+			if(partie==0){
+				Display_TestBegin(3);
+			}
+			else if(partie==1){
+				gettimeofday(&time_actuel, NULL);
+				if(check ==0)
+				circle = Display_Circle (circle);
+				else{
+					circle.x = 0;
+					circle.y = 0;
+					circle = Display_Circle (circle);
+					check=0;
+					score ++;
+				}
+				Display_TestScore(score);
+				Display_TestTime(time_debut, time_actuel);
+				if(  (long)(  ((( time_actuel.tv_sec - time_debut.tv_sec) * 1000000) + time_actuel.tv_usec) - (time_debut.tv_usec))/1000000 >= 15){
+					partie++;
+				}
+			}
+			else{
+				Display_TestEnd(score);
+				if(saving==0){
+					SaveScore(score,name,"../Save/TestBoule.txt");
+					saving=1;
+				}
+
+
+			}
+			break;
+			case 22:
+			gettimeofday(&time_actuel, NULL);
+
+			Display_TestTop();
+			Display_TestName("Test de reaction");
+
+			switch (partie) {
+				case 0:
+				Display_TestBegin(6);
+				break;
+				case 1:
+				if(  (long)(  ((( time_actuel.tv_sec - time_debut.tv_sec) * 1000000) + time_actuel.tv_usec) - (time_debut.tv_usec))/1000000 >= rdtime){
+					partie++;
+					gettimeofday(&time_debut, NULL);
+				}
+				break;
+				case 2:
+
+				effaceFenetre(0,170,0);
+				Display_TestTop();
+				Display_TestName("Test de reaction");
+				break;
+				case 3:
+				Display_TestEnd(score);
+
+				if(saving==0){
+					SaveScore(score,name,"../Save/TestReact.txt");
+					saving=1;
+				}
+				break;
+			}
+			break;
+			break;
+			case 3:
+			menuSync();
+			break;
+
+			case 31:
+			gettimeofday(&time_actuel, NULL);
+
+			Display_TestTop();
+			Display_TestName("Test de Q and M");
+
+			switch (partie) {
+				case 0:
+				Display_TestBegin(4);
+				break;
+				case 1:
+				Display_TestScore(score);
+				display_command(partie);
+				if(  (long)(  ((( time_actuel.tv_sec - time_debut.tv_sec) * 1000000) + time_actuel.tv_usec) - (time_debut.tv_usec))/1000000 >= 3){
+					partie++;
+					gettimeofday(&time_debut, NULL);
+				}
+				break;
+				case 2:
+				display_command(active);
+				listen=1;
+				time_actuel.tv_sec = time_actuel.tv_sec -10;
+				Display_TestTime(time_actuel, time_debut);
+				time_actuel.tv_sec = time_actuel.tv_sec +10;
+
+
+				Display_TestScore(score);
+				if(  (long)(  ((( time_actuel.tv_sec - time_debut.tv_sec) * 1000000) + time_actuel.tv_usec) - (time_debut.tv_usec))/1000000 >= 10){
+					partie++;
+				}
+				break;
+				case 3:
+				listen=0;
+				Display_TestEnd(score);
+				if(saving==0){
+					SaveScore(score,name,"../Save/TestSynchro.txt");
+					saving=1;
+				}
+				break;
+			}
+			break;
+			case 32:
+			Display_TestTop();
+			Display_TestName("Test du TextCouleur");
+			if(partie==0){
+				Display_TestBegin(5);
+			}
+			else if(partie==1){
+				gettimeofday(&time_actuel, NULL);
+				afficheJF();
+				affecritcouleur(ecritcouleur,c,ce,couleurecrit);
+
+
+				if(  (long)(  ((( time_actuel.tv_sec - time_niveau.tv_sec) * 1000000) + time_actuel.tv_usec) - (time_niveau.tv_usec))/1000000 >= 2){
+					ce=rand()%4;
+					c=rand()%4;
+
+					gettimeofday(&time_niveau, NULL);
+
+				}
+
+
+				Display_TestScore(score);
+				Display_TestTime(time_debut, time_actuel);
+				if(  (long)(  ((( time_actuel.tv_sec - time_debut.tv_sec) * 1000000) + time_actuel.tv_usec) - (time_debut.tv_usec))/1000000 >= 30){
+					partie++;
+				}
+			}
+			else{
+				Display_TestEnd(score);
+				if(saving==0){
+					SaveScore(score,name,"../Save/TestTextCouleur.txt");
+					saving=1;
+				}
+
+
+			}
+			break;
+			case 4:
+			PageResultat(name);
+			break;
+			case 888:
+			page=-2;
+			listen=1;
+			strcpy(name,"");
+			break;
+
+			case 999:
+
+
+			libereDonneesImageRGB(&image); /* On libere la structure image,
+			c'est plus propre, meme si on va sortir du programme juste apres */
+			termineBoucleEvenements();
+			freefct2(matrice);
+			break;
+		}
+
+
+		break;
+
+		case Clavier:
+		printf("%c : ASCII %d\n", caractereClavier(), caractereClavier());
+
+		if(listen!=1){
+			switch (caractereClavier())
+			{
+				case 'E': /* Pour sortir quelque peu proprement du programme */
+				case 'e':
+
+				libereDonneesImageRGB(&image); /* On libere la structure image,
+				c'est plus propre, meme si on va sortir du programme juste apres */
+				termineBoucleEvenements();
+				freefct2(matrice);
+				break;
+
+				case 'F':
+				case 'f':
+				pleinEcran = !pleinEcran; // Changement de mode plein ecran
+				if (pleinEcran)
+				modePleinEcran();
+				else
+				redimensionneFenetre(LargeurFenetre, HauteurFenetre);
+				break;
+
+				case 'R':
+				case 'r':
+				// Configure le systeme pour generer un message Temporisation
+				// toutes les 20 millisecondes (rapide)
+				demandeTemporisation(20);
+				break;
+
+				case 'L':
+				case 'l':
+				// Configure le systeme pour generer un message Temporisation
+				// toutes les 100 millisecondes (lent)
+				demandeTemporisation(100);
+				break;
+
+				case 'S':
+				case 's':
+				// Configure le systeme pour ne plus generer de message Temporisation
+				demandeTemporisation(-1);
+				break;
+
+			}
+		}
+		else{
+			if(page == -2){
+				if(partie==0){
+					text = caractereClavier();
+					if(caractereClavier()==13){
+						partie++;
+					}
+					else
+					strncat(name, &text, 1);
+				}
+
+				else if(partie ==1){
+					page=0;
+					partie=0;
+					listen=0;
+				}
+			}
+			if(page==31){
+				if(CheckSynchro(active)==1){
+					score++;
+					active = rand()%2 +4;
+				}
+				else if(CheckSynchro(active)==2){
+					score= score-2;
+					active = rand()%2 +4;
+				}
+			}
+		}
+		break;
+		case ClavierSpecial:
+		printf("ASCII %d\n", toucheClavier());
+		break;
+
+		case BoutonSouris:
+		if (etatBoutonSouris() == GaucheAppuye)
+		{
+			printf("Bouton gauche appuye en : (%d, %d)\n", abscisseSouris(), ordonneeSouris());
+
+			//Switch pour les différentes fonctions check
+
+			switch (page) {
+				case 0:
+				page = checkMenu();
+				break;
+				case 1:
+				page = checkMem();
+				break;
+				case 11:
+				if(partie==0){
+					check = Check_TestBegin();
+					if (check==1) {
+						partie++;
+						gettimeofday(&time_debut, NULL);
+						gettimeofday(&time_niveau, NULL);
+						score = 0;
+						niveau = 1;
+						Create_Random_Pattern(pattern, niveau);
+						avancee=0;
+					}
+				}
+				else if(partie==1){
+
+				}
+				else if(partie ==2){
+					printf("niveau = %d\n",niveau );
+					printf("partie = %d\n",partie );
+					printf("avancee = %d\n",avancee );
+					printf("pattern[avancee] = %d\n", pattern[avancee]);
+					if(check_Pattern(pattern[avancee])==1){
+						score++;
+						avancee++;
+						if(pattern[avancee]==0){
+							partie--;
+							avancee=0;
+							niveau ++;
+							Create_Random_Pattern(pattern, niveau);
+							gettimeofday(&time_niveau, NULL);
+						}
+					}
+					else{
+						partie++;
+						niveau=1;
+						avancee=0;
+						Create_Random_Pattern(pattern, niveau);
+
+					}
+
+				}
+				else{
+					if (Check_TestQuitter()==1) {
+						page = 1;
+						partie = 0;
+						score = -1;
+						saving=0;
+						avancee=0;
+						niveau=1;
+					}
+					else if (Check_TestRejouer()==1) {
+						partie = 1;
+						score = 0;
+						gettimeofday(&time_niveau, NULL);
+						gettimeofday(&time_debut, NULL);
+
+						saving=0;
+						avancee=0;
+						niveau=1;
+					}
+
+				}
+				break;
+				case 12:
+
+				if(partie==0){
+					check = Check_TestBegin();
+
+					if (check==1) {
+						partie++;
+						gettimeofday(&time_debut, NULL);
+						gettimeofday(&time_niveau, NULL);
+						score = 0;
+						matrice = chasard();
+						NbrOneMatrice = nbr_one_matrice(matrice);
+						printf("One = %d\n",NbrOneMatrice);
+						init_tab(tab);
+					}
+
+				}
+
+				else if(partie==1){
+
+
+				}
+				else if(partie==2){
+
+					verification(tab);
+					win_game = victoire(matrice,tab);
+
+
+					if (win_game == NbrOneMatrice){
+						partie = 1;
+						score++;
+						gettimeofday(&time_niveau, NULL);
+						matrice = chasard();
+						NbrOneMatrice = nbr_one_matrice(matrice);
+						printf("One = %d\n",NbrOneMatrice);
+						init_tab(tab);
+
+					}
+
+					else if (win_game >= NbrOneMatrice ){
+						partie++;
+					}
+
+				}
+				else{
+					if (Check_TestQuitter()==1) {
+						page = 1;
+						partie = 0;
+
+						score = -1;
+						saving=0;
+					}
+					else if (Check_TestRejouer()==1) {
+						partie = 0;
+						score = 0;
+						saving=0;
+						gettimeofday(&time_niveau, NULL);
+						gettimeofday(&time_debut, NULL);
+
+					}
+
+				}
+				break;
+				case 2:
+				page = checkFlex();
+				break;
+				case 21:
+				check = Check_Circle(circle);
+
+
+				if(partie==0){
+					check = Check_TestBegin();
+					if (check==1) {
+						partie++;
+						gettimeofday(&time_debut, NULL);
+					}
+				}
+				else if(partie==1){
+					check = Check_Circle(circle);
+
+				}
+				else{
+					if (Check_TestQuitter()==1) {
+						page = 2;
+						partie = 0;
+						score = -1;
+						saving=0;
+					}
+					else if (Check_TestRejouer()==1) {
+						partie = 1;
+						score = 0;
+						gettimeofday(&time_debut, NULL);
+						saving=0;
+
+					}
+
+				}
+				break;
+				case 22:
+				if(partie==0){
+					check = Check_TestBegin();
+					if (check==1) {
+						partie ++;
+						gettimeofday(&time_debut, NULL);
+						rdtime = rand()%4;
+					}
+				}
+				else if(partie==2){
+					check = Check_TestBegin();
+					if (check==1) {
+						partie ++;
+						gettimeofday(&time_niveau, NULL);
+						score=  (((( time_niveau.tv_sec - time_debut.tv_sec) * 1000000) + time_niveau.tv_usec) - (time_debut.tv_usec))/1000;
+
+					}
+				}
+				else{
+					if (Check_TestQuitter()==1) {
+						page = 2;
+						partie = 0;
+						saving=0;
+					}
+					else if (Check_TestRejouer()==1) {
+						partie = 1;
+						score = 0;
+						gettimeofday(&time_debut, NULL);
+						saving=0;
+						rdtime = rand()%4;
+
+
+					}
+
+				}
+				break;
+				break;
+				case 3:
+				page = checkSync();
+				partie=0;
+				score=0;
+				break;
+
+				case 31:
+				if(partie==0){
+					check = Check_TestBegin();
+					if (check==1) {
+						partie ++;
+						gettimeofday(&time_debut, NULL);
+						score=0;
+					}
+				}
+				else if(partie==1){
+
+
+				}
+				else if(partie==2){
+
+
+
+
+				}
+				else{
+					if (Check_TestQuitter()==1) {
+						page = 3;
+						partie = 0;
+						score = -1;
+						saving=0;
+					}
+					else if (Check_TestRejouer()==1) {
+						partie = 1;
+						score = 0;
+						gettimeofday(&time_debut, NULL);
+						saving=0;
+
+					}
+
+				}
+				break;
+				case 32:
+
+				if(partie==0){
+					check = Check_TestBegin();
+					if (check==1) {
+						partie++;
+						ce=rand()%4;
+						c=rand()%4;
+						gettimeofday(&time_debut, NULL);
+						gettimeofday(&time_niveau, NULL);
+					}
+				}
+				else if(partie==1){
+					if (point_duocouleur( c , ce)==1) {
+						score++;
+						gettimeofday(&time_niveau, NULL);
+						ce=rand()%4;
+						c=rand()%4;
+					}
+					else if (point_duocouleur( c , ce)==-1) {
+						score--;
+						gettimeofday(&time_niveau, NULL);
+						ce=rand()%4;
+						c=rand()%4;
+					}
+				}
+				else{
+					if (Check_TestQuitter()==1) {
+						page = 3;
+						partie = 0;
+						score = -1;
+						saving=0;
+					}
+					else if (Check_TestRejouer()==1) {
+						partie = 1;
+						score = 0;
+						gettimeofday(&time_debut, NULL);
+						saving=0;
+
+					}
+
+				}
+
+				break;
+				case 4:
+				page = CheckLeave();
+				break;
+			}
+		}
+		else if (etatBoutonSouris() == GaucheRelache)
+		{
+			printf("Bouton gauche relache en : (%d, %d)\n", abscisseSouris(), ordonneeSouris());
+		}
+		break;
+
+		case Souris: // Si la souris est deplacee
+		break;
+
+		case Inactivite: // Quand aucun message n'est disponible
+		break;
+
+		case Redimensionnement: // La taille de la fenetre a ete modifie ou on est passe en plein ecran
+		// Donc le systeme nous en informe
+
+		printf("Largeur : %d\t", largeurFenetre());
+		printf("Hauteur : %d\n", hauteurFenetre());
+		break;
+	}
+}
---
+++ PROJET_SOURCE_GR_3/Code/menu.c
@@ -0,0 +1,366 @@
+#include "main.h"
+
+
+/**
+ * \file menu.c
+ * \brief Fonctions affichage des menus et leur hitbox.
+ * \author Thomas.B
+ * \version 1.0
+ * \date 25 Juin 2020
+ *
+ */
+
+
+ /**
+ * \fn void traceMenu (char pName [MAX_NAME])
+ * \brief Affichage menu principal
+ *
+ * \param Nom de la personne connectée
+ * \return none
+ */
+
+
+void traceMenu(char pName[MAX_NAME]) {
+	float taille2, taille3, taille4,taille5,taille6,taille7;
+	//triangle central
+	couleurCourante(213, 136, 120);
+	triangle(375, 400, 625, 400, 500, 200);
+	//triangle haut
+	couleurCourante(175, 170, 169);
+	triangle(375, 400, 625, 400, 500, 600);
+	//triangle droite
+	couleurCourante(183, 248, 125);
+	triangle(500, 200, 625, 400, 750, 200);
+	//triangle gauche
+	couleurCourante(114, 192, 231);
+	triangle(500, 200, 375, 400, 250, 200);
+	couleurCourante(214, 214, 214);
+	//rectangle d'affichage du texte
+	rectangle(300, 800, 700, 750);
+	//titre
+	Display_TestTop();
+	Display_TestName("Test Checker");
+	//bouton quitter
+	couleurCourante(220,220,220);
+	rectangle(LargeurFenetre-60,0,LargeurFenetre,40);
+	//bouton identification
+	couleurCourante(220,220,220);
+	rectangle(0,0,80,40);
+	//bouton connecté
+	couleurCourante(220,220,220);
+	rectangle(300,100,700,150);
+	//titre triangle haut
+	couleurCourante(0, 0, 0);
+	taille2 = tailleChaine("Memoire", 24);
+	afficheChaine("Memoire", 24, (LargeurFenetre - taille2)/2, 462);
+	//titre triangle central
+	couleurCourante(0, 0, 0);
+	taille3 = tailleChaine("Resultats", 24);
+	afficheChaine("Resultats", 24, (LargeurFenetre - taille3)/2, 312);
+	//titre triangle droite
+	couleurCourante(0, 0, 0);
+	afficheChaine("Reflexes", 24, 580, 262);
+	//titre triangle gauche
+	couleurCourante(0, 0, 0);
+	afficheChaine("Synchro", 24, 322, 262);
+	//titre quitter
+	couleurCourante(0, 0, 0);
+	taille4 = tailleChaine("Quitter", 15);
+	afficheChaine("Quitter", 15,(LargeurFenetre-60)+(60-taille4)/2, 15);
+	//titre identification
+	couleurCourante(0, 0, 0);
+	taille5 = tailleChaine("Identification", 12);
+	afficheChaine("Identification", 12,(80-taille5)/2, 15);
+	//titre Connecté
+	couleurCourante(0, 0, 0);
+	taille6 = tailleChaine("Connecte en tant que :", 15);
+	taille7 = tailleChaine(pName, 15);
+	afficheChaine("Connecte en tant que :", 15,300+(400-(taille6+taille7+15))/2, 120);
+	afficheChaine(pName, 15,(300+(400-(taille6+taille7))/2)+taille6+15, 120);
+}
+
+/**
+ * \fn int checkMenu ()
+ * \brief hitbox menu principal
+ *
+ * \param none
+ * \return adresse de la page choisie
+ */
+
+int checkMenu() {
+	int active = 0;
+	//triangle memoire
+	if(abscisseSouris() >= 375 && abscisseSouris() <= 625 && ordonneeSouris() >= 400 && ordonneeSouris() <= 600) {
+		active = 1;
+		printf("Menu Mémoire\n");
+	}
+	//triangle Reflexes
+	if(ordonneeSouris() <= 310 && ordonneeSouris() >= 200 && abscisseSouris() >= 570 && abscisseSouris() <= 750) {
+		active = 2;
+		printf("Menu Réflexes\n");
+	}
+	//triangle synchro
+	if(ordonneeSouris() <= 310 && ordonneeSouris() >= 200 && abscisseSouris() >= 250 && abscisseSouris() <= 430) {
+		active = 3;
+		printf("Menu Synchro\n");
+	}
+	//triangle Resultats
+	if(abscisseSouris() >= 440 && ordonneeSouris() >= 300 && abscisseSouris() <= 550 && ordonneeSouris() <= 350){
+		active = 4;
+		printf("Menu Résultats\n");
+	}
+	if(abscisseSouris() <= 593 && ordonneeSouris() >= 350 && abscisseSouris() >= 409 && ordonneeSouris() <= 400){
+		active = 4;
+		printf("Menu Résultats\n");
+	}
+	//bouton quitter
+	if(abscisseSouris() <= LargeurFenetre && ordonneeSouris() >= 0 && abscisseSouris() >= LargeurFenetre-60 && ordonneeSouris() <= 40){
+		active = 999;
+		printf("Programme fermé\n");
+	}
+	//bouton identification
+	if(abscisseSouris() <= 80 && ordonneeSouris() >= 0 && abscisseSouris() >= 0 && ordonneeSouris() <= 40){
+		active = 888;
+		printf("Choix d'un nouvel identifiant...\n");
+	}
+
+	return active;
+}
+
+ /**
+ * \fn void menuFlex ()
+ * \brief Affichage menu reflex
+ *
+ * \param none
+ * \return none
+ */
+
+void menuFlex() {
+	float taille;
+	//bandeau
+	Display_TestTop();
+	Display_TestName("Menu Reflexes");
+	//triangle central
+	couleurCourante(213, 136, 120);
+	triangle(375, 500, 625, 500, 500, 300);
+	//triangle gauche
+	couleurCourante(175, 170, 169);
+	triangle(400, 200, 275, 400, 150, 200);
+	//triangle droite
+	couleurCourante(183, 248, 125);
+	triangle(600, 200, 725, 400, 850, 200);
+	//titre triangle central
+	couleurCourante(0, 0, 0);
+	taille = tailleChaine("Fast Click", 24);
+	afficheChaine("Fast Click", 24, (LargeurFenetre - taille)/2, 422);
+	//titre triangle gauche
+	couleurCourante(0, 0, 0);
+	afficheChaine("Reaction", 24, 232, 262);
+	//titre triangle droite
+	couleurCourante(0, 0, 0);
+	afficheChaine("Resultats", 24, 682, 262);
+	//bouton retour
+	couleurCourante(210, 210, 210);
+	rectangle(0, 710, 50, 680);
+	couleurCourante(175, 170, 169);
+	triangle(2, 695, 12, 708, 12, 682);
+	rectangle(10, 702, 48, 688);
+}
+
+ /**
+ * \fn int checkFlex()
+ * \brief hitbox menu reflex
+ *
+ * \param none
+ * \return adresse page choisie
+ */
+
+int checkFlex() {
+	int active = 2;
+	//triangle central
+	if(abscisseSouris() >= 375 && abscisseSouris() <= 625 && ordonneeSouris() <= 500 && ordonneeSouris() >= 300) {
+		active = 21;
+		printf("Vers Fast Click\n");
+	}
+	//triangle gauche
+	if(ordonneeSouris() <= 310 && ordonneeSouris() >= 200 && abscisseSouris() >= 570 && abscisseSouris() <= 750) {
+		active = 4;
+		printf("Vers Résultats\n");
+	}
+	//triangle droite
+	if(ordonneeSouris() <= 310 && ordonneeSouris() >= 200 && abscisseSouris() >= 250 && abscisseSouris() <= 430) {
+		active = 22;
+		printf("Vers Test 2\n");
+	}
+	//bouton retour
+	if(abscisseSouris() <= 50 && abscisseSouris() >= 0 && ordonneeSouris() <= 710 && ordonneeSouris() >= 680){
+		active = 0;
+		printf("Vers Menu");
+	}
+	return active;
+}
+
+/**
+ * \fn int CheckLeave ()
+ * \brief hitbox boutton quitter page resultat
+ *
+ * \param none
+ * \return adresse menu principal
+ */
+
+int CheckLeave() {
+	int active = 4;
+	//Bouton quitter
+	if(abscisseSouris() >= 0 && abscisseSouris() <= 50 && ordonneeSouris() >= HauteurFenetre-30 && ordonneeSouris() <= HauteurFenetre) {
+		active = 0;
+		printf("Retour Menu principal\n");
+	}
+	return active;
+}
+
+ /**
+ * \fn void menuMem ()
+ * \brief Affichage menu memoire
+ *
+ * \param none
+ * \return none
+ */
+
+void menuMem() {
+	float taille;
+	//bandeau
+	Display_TestTop();
+	Display_TestName("Menu Memoire");
+	//triangle central
+	couleurCourante(213, 136, 120);
+	triangle(375, 500, 625, 500, 500, 300);
+	//triangle gauche
+	couleurCourante(175, 170, 169);
+	triangle(400, 200, 275, 400, 150, 200);
+	//triangle droite
+	couleurCourante(183, 248, 125);
+	triangle(600, 200, 725, 400, 850, 200);
+	//titre triangle central
+	couleurCourante(0, 0, 0);
+	taille = tailleChaine("Simon Says", 24);
+	afficheChaine("Simon Says", 24, (LargeurFenetre - taille)/2, 425);
+	//titre triangle gauche
+	couleurCourante(0, 0, 0);
+	afficheChaine("Pattern", 24, 232, 262);
+	//titre triangle droite
+	couleurCourante(0, 0, 0);
+	afficheChaine("Resultats", 24, 682, 262);
+	//bouton retour
+	couleurCourante(210, 210, 210);
+	rectangle(0, 710, 50, 680);
+	couleurCourante(175, 170, 169);
+	triangle(2, 695, 12, 708, 12, 682);
+	rectangle(10, 702, 48, 688);
+}
+
+/**
+ * \fn int checkFlex()
+ * \brief hitbox menu memoire
+ *
+ * \param none
+ * \return adresse test choisie
+ */
+
+
+int checkMem() {
+	int active = 1;
+	//triangle central
+	if(abscisseSouris() >= 375 && abscisseSouris() <= 625 && ordonneeSouris() <= 500 && ordonneeSouris() >= 300) {
+		active = 11;
+		printf("Vers Test 1\n");
+	}
+	//triangle gauche
+	if(ordonneeSouris() <= 310 && ordonneeSouris() >= 200 && abscisseSouris() >= 570 && abscisseSouris() <= 750) {
+		active = 4;
+		printf("Vers Résultats\n");
+	}
+	//triangle droite
+	if(ordonneeSouris() <= 310 && ordonneeSouris() >= 200 && abscisseSouris() >= 250 && abscisseSouris() <= 430) {
+		active = 12;
+		printf("Vers Pattern\n");
+	}
+	//bouton retour
+	if(abscisseSouris() <= 50 && abscisseSouris() >= 0 && ordonneeSouris() <= 710 && ordonneeSouris() >= 680){
+		active = 0;
+		printf("Vers Menu");
+	}
+	return active;
+}
+
+/**
+ * \fn void menuSync()
+ * \brief Affichage menu synchro
+ *
+ * \param none
+ * \return none
+ */
+
+void menuSync() {
+	float taille;
+	//bandeau
+	Display_TestTop();
+	Display_TestName("Menu Synchronisation");
+	//triangle central
+	couleurCourante(213, 136, 120);
+	triangle(375, 500, 625, 500, 500, 300);
+	//triangle gauche
+	couleurCourante(175, 170, 169);
+	triangle(400, 200, 275, 400, 150, 200);
+	//triangle droite
+	couleurCourante(183, 248, 125);
+	triangle(600, 200, 725, 400, 850, 200);
+	//titre triangle central
+	couleurCourante(0, 0, 0);
+	taille = tailleChaine("Texte Couleur", 24);
+	afficheChaine("Texte Couleur", 24, (LargeurFenetre - taille)/2, 422);
+	//titre triangle gauche
+	couleurCourante(0, 0, 0);
+	afficheChaine("Q et M", 24, 228, 262);
+	//titre triangle droite
+	couleurCourante(0, 0, 0);
+	afficheChaine("Resultats", 24, 682, 262);
+	//bouton retour
+	couleurCourante(210, 210, 210);
+	rectangle(0, 710, 50, 680);
+	couleurCourante(175, 170, 169);
+	triangle(2, 695, 12, 708, 12, 682);
+	rectangle(10, 702, 48, 688);
+}
+
+/**
+ * \fn int checkSynch ()
+ * \brief hitbox menu Synchro
+ *
+ * \param none
+ * \return adresse test choisie
+ */
+
+int checkSync() {
+	int active = 3;
+	//triangle central
+	if(abscisseSouris() >= 375 && abscisseSouris() <= 625 && ordonneeSouris() <= 500 && ordonneeSouris() >= 300) {
+		active = 32;
+		printf("Q et M\n");
+	}
+	//triangle gauche
+	if(ordonneeSouris() <= 310 && ordonneeSouris() >= 200 && abscisseSouris() >= 570 && abscisseSouris() <= 750) {
+		active = 4;
+		printf("Vers Résultats\n");
+	}
+	//triangle droite
+	if(ordonneeSouris() <= 310 && ordonneeSouris() >= 200 && abscisseSouris() >= 250 && abscisseSouris() <= 430) {
+		active = 31;
+		printf("Text Couleur\n");
+	}
+	//bouton retour
+	if(abscisseSouris() <= 50 && abscisseSouris() >= 0 && ordonneeSouris() <= 710 && ordonneeSouris() >= 680){
+		active = 0;
+		printf("Vers Menu");
+	}
+	return active;
+}
